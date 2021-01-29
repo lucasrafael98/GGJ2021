c7f4274523b238e0b0d260ad8e9a23de529aa65c
unknown <joaolucasrafael@gmail.com>
2021-01-29 23:49:05 +0000

    Food types and attributes.

---
+++ GGJ2021/Assets/TopDownEngine/ThirdParty/MoreMountains/InventoryEngine/InventoryEngine/Scripts/Items/FoodItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using MoreMountains.Tools;
+using System;
+
+namespace MoreMountains.InventoryEngine
+{	
+	[CreateAssetMenu(fileName = "FoodItem", menuName = "MoreMountains/InventoryEngine/FoodItem", order = 0)]
+	[Serializable]
+	/// <summary>
+	/// Base item class, to use when your object doesn't do anything special
+	/// </summary>
+	public class FoodItem : InventoryItem 
+	{
+		public enum Type{Vegetable, Protein, Carb}
+		public enum Attr{Spicy, Juicy, Dry, Sweet, Fruit}
+
+		public Type _type;
+		public Attr _attr;
+	}
+}
