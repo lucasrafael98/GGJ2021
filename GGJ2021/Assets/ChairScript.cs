b3f1f5d149f44b04170686b22d0130cede202198
nytrum <francisco.campanico@tecnico.ulisboa.pt>
2021-01-30 16:59:50 +0000

    I hope this works

---
+++ GGJ2021/Assets/ChairScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairScript : MonoBehaviour
+{
+    [Header("Chair has a client on it")]
+    public bool _isOccupied = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Client"))
+        {
+            _isOccupied = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Client"))
+        {
+            _isOccupied = false;
+        }
+    }
+}
---
+++ GGJ2021/Assets/ClientScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Pathfinding;
+
+public class ClientScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected List<GameObject> shuffledList;
+    void Start()
+    {
+        GameObject[] chairs = GameObject.FindGameObjectsWithTag("Chair");
+        List<GameObject> childObjects = new List<GameObject>();
+        foreach (GameObject chair in chairs)
+        {
+            childObjects.Add(chair);
+        }
+        shuffledList = childObjects.OrderBy(x => Random.value).ToList();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.GetComponent<AIDestinationSetter>().target == null)
+        {
+            getAChair();
+        }   
+    }
+
+    void getAChair()
+    {
+        foreach (GameObject go in shuffledList)
+        {
+            ChairScript temp = go.GetComponent<ChairScript>();
+            if (!go.GetComponent<ChairScript>()._isOccupied)
+            {
+                this.gameObject.GetComponent<AIDestinationSetter>().target = go.transform;
+                return;
+            }
+        }
+    }
+}
