638a8415e1f001264eb82c79c2575a975cb0fd06
nytrum <francisco.campanico@tecnico.ulisboa.pt>
2021-01-31 02:49:51 +0000

    Menus working!

--- GGJ2021/Assets/TopDownEngine/Common/Scripts/Managers/SatisfactionBar.cs
+++ GGJ2021/Assets/TopDownEngine/Common/Scripts/Managers/SatisfactionBar.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class SatisfactionBar : MonoBehaviour
 {
     public Slider slider;
+
+    public GameObject GameOverMenu;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,10 @@ public class SatisfactionBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(slider.value <= 0)
+        {
+            GameOverMenu.SetActive(true);
+        }
     }
 
     public void ChangeSlider(float variation)
